f10b80ad7dbf8f581a9855ea3a1b56e397cecd4f
Abdelrahman Mostafa <112385425+Abdouumostafa@users.noreply.github.com>
2023-08-19 13:26:29 +0300

    Update Billing.jsx

--- my-project/src/components/Billing.jsx
+++ my-project/src/components/Billing.jsx
@@ -7,7 +7,7 @@ const Billing = () => {
       <div className={`${layout.sectionImgReverse}`}>
         <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]"/>
       </div>
-
+ 
       <div className={`${layout.sectionInfo}`}>
         <h2 className={`${styles.heading2}`}>Easily control your <br className="sm:block hidden"/> billing & invoicing.</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea placerat.</p>
